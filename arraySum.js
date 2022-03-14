df673f62f96e7e852b0ba9dad82226fcbc1e3da8
BIPLOB <biplob2358@gmail.com>
2022-03-14 11:19:48 +0600

    add

---
+++ arraySum.js
@@ -0,0 +1,7 @@
+var numbers = [40, 55, 21, 65, 99, 55, 77, 33, 56, 87];
+var sum = 0;
+for (var i = 0; i < numbers.length; i++) {
+  var element = numbers[i];
+  sum = sum + element;
+}
+console.log(sum);
